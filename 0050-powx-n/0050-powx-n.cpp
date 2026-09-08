a658be594bbf1ff90eebae150db17f33a9a80042
PRIYANSHU SHAKYA <priyanshushakya459@gmail.com>
2026-09-08 09:30:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.66%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       return pow(x,n);
+      long long m=n;
+       if(m==0)return 1;
+        if(x==0)return 0;
+      if(m<0){
+        m=-m;
+        x=1/x;
+
+      }
+      double ans=1.0;
+     
+      while(m){
+        if(m%2){
+        ans*=x;}
+        x=x*x;
+        m/=2;
+
+      }
+      return ans;
     }
 };
