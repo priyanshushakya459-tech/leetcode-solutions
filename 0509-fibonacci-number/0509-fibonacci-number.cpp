e6bbbb8a950d6e7e735f0ece30cb5f32198a6af6
PRIYANSHU SHAKYA <priyanshushakya459@gmail.com>
2026-09-11 21:52:14 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
-int f(int n,vector<int>&dp){
-     if(n==0||n==1)return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]= fib(n-1)+fib(n-2);
-}
+
     int fib(int n) {
-       vector<int>dp(n+1,-1);
-       return f(n,dp);
+        if(n==0||n==1)return n;
+      int sprev=0;
+      int prev=1;
+      int ans=0;
+      for(int i=2;i<=n;i++){
+        ans=(sprev+prev);
+        sprev=prev;
+        prev=ans;
+      }
+      return ans;
+     
     }
 };
