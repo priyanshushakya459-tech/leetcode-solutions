a42ed5f0eda02f9b09d3fec5cbf129d52d85d13f
PRIYANSHU SHAKYA <priyanshushakya459@gmail.com>
2026-09-12 11:12:50 +0530

    Time: 0 ms (100%), Space: 10.8 MB (30.12%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+void sd(vector<int>& nums,vector<vector<int>>&ans,vector<int>temp,int index){
+    ans.push_back(temp);
+    for(int i=index;i<nums.size();i++){
+        if(i!=index&&nums[i]==nums[i-1])continue;
+    temp.push_back(nums[i]);
+    sd(nums,ans,temp,i+1);
+    temp.pop_back();
+    }
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+       vector<vector<int>>ans;
+       vector<int>temp;
+        sd(nums,ans,temp,0);
+        return ans;
+    }
+};
