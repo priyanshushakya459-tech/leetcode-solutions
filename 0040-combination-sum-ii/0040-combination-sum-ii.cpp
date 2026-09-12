92e64f9a4a6fb177da9dbf316c48fc0db0a9e31c
PRIYANSHU SHAKYA <priyanshushakya459@gmail.com>
2026-09-12 09:13:25 +0530

    Time: 0 ms (100%), Space: 13.9 MB (84.35%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
- void cs(vector<int>& candidates, vector<vector<int>>& ans,
-            vector<int>& temp, int target, int index) {
-
-        if (target == 0) {
-            ans.push_back(temp);
-            return;
-        }
-
-        for (int i = index; i < candidates.size(); i++) {
-
-            // Duplicate skip
-            if (i > index && candidates[i] == candidates[i - 1])
-                continue;
-
-            if (candidates[i] > target)
-                break;
-
-            temp.push_back(candidates[i]);
-            cs(candidates, ans, temp, target - candidates[i], i + 1);
-            temp.pop_back();
-        }
+void cs(vector<int>& candidates,vector<vector<int>> &ans,vector<int>&temp,int target,int index){
+    
+    if(target==0){
+        ans.push_back(temp);
+        return ;
     }
+      // Duplicate skip
+            // if ( index>0 && candidates[index] == candidates[index - 1])
+            //     continue;
+
+    if(candidates.size()==index||target<0){
+        return ;
+    }
+   
+
+    temp.push_back(candidates[index]);
+    cs(candidates,ans,temp,target-candidates[index],index+1);
+    temp.pop_back();
+    // temp.push_back(cadidates[index]);
+    while(index+1<candidates.size()&&candidates[index]==candidates[index+1]){index++;
+
+    }
+    cs(candidates,ans,temp,target,index+1);
+   
+}
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(),candidates.end());
       
