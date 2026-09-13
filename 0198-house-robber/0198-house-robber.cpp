2c0eeedba936213ae63f465cc8bc98c7932ce702
PRIYANSHU SHAKYA <priyanshushakya459@gmail.com>
2026-09-13 08:11:20 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -7,10 +7,9 @@ public:
       if(n==1)return dp[0];
       dp[1]=max(nums[0],nums[1]);
       if(n==2)return dp[1];
-    dp[2]=max(nums[0]+nums[2],nums[1]);
-    if(n==3)return dp[2];
-      for(int i=3;i<n;i++){
-        dp[i]=max(nums[i]+dp[i-2],nums[i]+dp[i-3]);
+    
+      for(int i=2;i<n;i++){
+        dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
       }
      
       return max(dp[n-2],dp[n-1]);
